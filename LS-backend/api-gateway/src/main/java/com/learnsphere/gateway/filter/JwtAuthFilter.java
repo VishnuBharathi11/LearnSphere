5ff7f23b3d86d88a132d27aaea7942cc68203e56
Anbu02042006 <717823s105@kce.ac.in>
2026-02-18 12:20:41 +0530

    pull req

---
+++ LS-backend/api-gateway/src/main/java/com/learnsphere/gateway/filter/JwtAuthFilter.java
@@ -0,0 +1,70 @@
+package com.learnsphere.gateway.filter;
+
+import com.learnsphere.gateway.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter implements GlobalFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange,
+                             org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
+
+        String path = exchange.getRequest().getURI().getPath();
+
+        // Allow auth endpoints
+        if (path.contains("/auth/login") ||
+            path.contains("/auth/register")) {
+            return chain.filter(exchange);
+        }
+
+        String authHeader =
+                exchange.getRequest().getHeaders()
+                        .getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+
+        try {
+
+            Claims claims = jwtUtil.validateToken(token);
+            String role = claims.get("role", String.class);
+
+            // 🔥 ROLE ACCESS CONTROL
+            if (path.startsWith("/admin") && !"ADMIN".equals(role)) {
+                exchange.getResponse()
+                        .setStatusCode(HttpStatus.FORBIDDEN);
+                return exchange.getResponse().setComplete();
+            }
+
+            if (path.startsWith("/payments") &&
+                    !(role.equals("STUDENT") || role.equals("ADMIN"))) {
+
+                exchange.getResponse()
+                        .setStatusCode(HttpStatus.FORBIDDEN);
+                return exchange.getResponse().setComplete();
+            }
+
+        } catch (Exception e) {
+            exchange.getResponse()
+                    .setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+}
